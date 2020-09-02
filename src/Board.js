c104ef7775efc82183dfb60fe02d546e1337adca
Hitesh Balar <hitesh.balar@jda.com>
2020-09-02 18:24:25 +0530

    Changes board/square game logic

--- src/Board.js
+++ src/Board.js
@@ -1,42 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square/Sqaure';
-import { Dimensions, View } from 'react-native';
+import { View, Button, Text } from 'react-native';
 
-const DeviceWidth = Dimensions.get('window').width;
+const winningCombination = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const emptyBoard = ['', '', '', '', '', '', '', '', ''];
 
 const Board = () => {
+  const [turn, setTurn] = useState('X');
+  const [win, setWin] = useState(null);
+  const [board, setBoard] = useState([...emptyBoard]);
+
+  const handleClick = (id) => {
+    updateState(id);
+  };
+
+  const updateState = (id) => {
+    let updateBoard = [...board];
+    updateBoard[id] = turn;
+    setBoard(updateBoard);
+    setTurn(turn === 'X' ? 'O' : 'X');
+    setWin(checkWin(updateBoard));
+  };
+
+  const reset = () => {
+    setBoard([...emptyBoard]);
+    setTurn('X');
+    setWin(null);
+  };
+
+  const checkWin = (updateBoard) => {
+    let winner = null;
+    winningCombination.forEach((comb, index) => {
+      if (
+        updateBoard[comb[0]] &&
+        updateBoard[comb[0]] === updateBoard[comb[1]] &&
+        updateBoard[comb[0]] === updateBoard[comb[2]]
+      )
+        winner = updateBoard[comb[0]];
+    });
+    return winner ? winner : updateBoard.includes('') ? null : 'T';
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <View
-        style={{
-          flexDirection: 'row',
-          backgroundColor: 'grey',
-        }}
-      >
-        <View>
-          <Square />
-          <Square />
-          <Square />
-        </View>
-        <View>
-          <Square />
-          <Square />
-          <Square />
-        </View>
-        <View>
-          <Square />
-          <Square />
-          <Square />
-        </View>
+    <View style={styles.container}>
+      <Text style={styles.header}>TicToe</Text>
+      <View style={styles.board}>
+        {board.map((text, id) => (
+          <Square
+            text={text}
+            id={id}
+            key={'square' + id}
+            handleClick={handleClick}
+          />
+        ))}
+      </View>
+      <Text style={styles.title}>
+        {(win === 'X' || win === 'O') && win + ' won the game'}
+        {win === 'T' && 'Game Tie'}
+      </Text>
+      <View style={styles.reset}>
+        <Button title="Restart" onPress={reset} />
       </View>
     </View>
   );
+};
+
+const styles = {
+  container: {
+    marginVertical: 40,
+  },
+  board: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  header: {
+    textAlign: 'center',
+    marginVertical: 16,
+    fontSize: 21,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginVertical: 16,
+  },
+  reset: {
+    marginLeft: 20,
+    marginRight: 20,
+  },
 };
 
 export default Board;
--- src/Square/Sqaure.js
+++ src/Square/Sqaure.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react';
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
 import { Dimensions } from 'react-native';
+
 const DeviceWidth = Dimensions.get('window').width;
 
-const Square = () => {
-  const sqStyle = {
-    width: DeviceWidth * 0.3,
-    height: DeviceWidth * 0.3,
-    marginBottom: 1,
-    backgroundColor: randomHexColor(),
+const Square = ({ text, id, handleClick }) => {
+  const style = {
+    square: {
+      width: DeviceWidth * 0.3,
+      height: DeviceWidth * 0.3,
+      marginBottom: 5,
+      marginLeft: 5,
+      backgroundColor: randomHexColor(),
+    },
+    text: {
+      color: 'white',
+      fontSize: 50,
+      margin: 'auto',
+    },
+    tile: {
+      height: '100%',
+    },
   };
+
+  const squareClick = () => {
+    if (text) return;
+    handleClick(id);
+  };
+
   return (
-    <View style={sqStyle}>
-      <Button onPress={() => {}} title="" />
+    <View style={[style.square, { opacity: text ? 1 : 0.4 }]}>
+      <TouchableOpacity style={style.tile} onPress={() => squareClick()}>
+        <Text style={style.text}>{text}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
